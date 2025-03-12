efa6cd7cf826af93660347e465776c0a3e841bd7
Hunter1995 <zenyalyt@gmail.com>
2025-03-12 18:03:34 +0200

    Added conditional rendering

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,15 @@
 import './App.css'
 import MovieCard from './components/MovieCard'
 function App() {
+  const movieNumber = 1;
+  
   return (
     <>
-      <MovieCard movie={{title: "Tim's Film", release_date: "2024"}}/>
+      {movieNumber ===1 ? (
+        <MovieCard movie={{title: "Tim's Film", release_date: "2024"}}/>
+      ) : (
+        <MovieCard movie={{title: "Joes Film", release_date: "2020"}}/>
+      )}
     </>
   )
 }
