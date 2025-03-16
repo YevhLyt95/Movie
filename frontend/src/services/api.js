b416938d18bd2af9d1aecf9030ff57515a45b425
Hunter1995 <zenyalyt@gmail.com>
2025-03-16 17:49:21 +0200

    Added api and new components

---
+++ frontend/src/services/api.js
@@ -0,0 +1,13 @@
+const API_KEY = "319f20c83c876949950d36e4f2e92364";
+const BASE_URL = "https://api.themoviedb.org/3";
+
+export const getPopularMovies = async () => {
+    const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+    const data = await response.json();
+    return data.results;
+};
+export const searchMovies = async(query) => {
+    const response = await fetch(`${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(query)}`);
+    const data = await response.json();
+    return data.results;
+};
