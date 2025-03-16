414c96981a4c5855967a43372ef316cd99d6af3c
Hunter1995 <zenyalyt@gmail.com>
2025-03-16 21:59:26 +0200

    Posters added

--- frontend/src/components/MovieCard.jsx
+++ frontend/src/components/MovieCard.jsx
@@ -5,7 +5,7 @@ function MovieCard({movie}){
     }
     return <div className="movie-card">
         <div className="movie-poster">
-            <img src={movie.url} alt={movie.title}/>
+            <img src={`https://image.tmdb.org/t/p/w500/1E5baAaEse26fej7uHcjOgEE2t2.jpg`} alt={movie.title}/>
             <div className="movie-overlay">
                 <button className="favorite-btn" onClick={onFavoriteClick}>
                     ♥
@@ -14,7 +14,7 @@ function MovieCard({movie}){
         </div>
         <div className="movie-info">
             <h3>{movie.title}</h3>
-            <p>{movie.release_date}</p>
+            <p>{movie.release_date?.split("-")[0]}</p>
         </div>
     </div>
 }
